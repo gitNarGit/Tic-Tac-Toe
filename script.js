454fa34a531deb84beb4069e8fd14e6efe4739f2
Naniko <narine.boyakhchyan@gmail.com>
2018-03-22 23:03:46 +0400

    some fixes

--- script.js
+++ script.js
@@ -1,67 +1,69 @@
-function startGame(){
-
-    for(var i=1; i<=9; i++){
+startGame = () => {
+    for (let i = 1; i <= 9; i++) {
         clearBox(i);
     }
-
-  document.turn = "X";
-  if(Math.random() < 0.5){
-    document.turn = "O";
-  }
-  document.winner = null;
-  setMessage(document.turn + " ready to start.")
-}
-function setMessage(msg){
-  document.getElementById("message").innerText = msg;
-}
-function nextMove(square){
-    if(document.winner != null){
-      setMessage(document.winner + " already won the game!");
-    } else if(square.innerText == ""){
-    square.innerText = document.turn;
-    switchTurn();
-  } else{
-    setMessage("Hey, that square not yours!")
-  }
-}
-function switchTurn(){
-  if(checkWinner(document.turn)){
-    setMessage("Congratulations, " + document.turn + " You Win!");
-    document.winner = document.turn;
-  } else if(document.turn == "X"){
-    document.turn = "O";
-    setMessage("It's " + document.turn + "'s turn!");
-  } else{
     document.turn = "X";
-    setMessage("It's " + document.turn + "'s turn!");
-  }
+    Math.random() < 0.5 ? document.turn = "O" : null;
+    document.winner = null;
+    setMessage(document.turn + " ready to start.")
+};
 
-}
-function checkWinner(move){
-  var result = false;
-  if(checkRow(1,2,3,move) ||
-     checkRow(4,5,6,move) ||
-     checkRow(7,8,9,move) ||
-     checkRow(1,4,7,move) ||
-     checkRow(2,5,8,move) ||
-     checkRow(3,6,9,move) ||
-     checkRow(7,8,9,move) ||
-     checkRow(1,5,9,move) ||
-     checkRow(3,5,7,move)) {
+setMessage = (msg) => {
+    document.getElementById("message").innerText = msg;
+};
+
+nextMove = (square) => {
+    if (document.winner !== null) {
+        setMessage(document.winner + " already won the game!");
+    } else if (square.innerText === "") {
+        square.innerText = document.turn;
+        switchTurn();
+    } else {
+        setMessage("Hey, that square not yours!")
+    }
+};
+
+switchTurn = () => {
+    if (checkWinner(document.turn)) {
+        setMessage("Congratulations, " + document.turn + " You Win!");
+        document.winner = document.turn;
+    } else if (document.turn === "X") {
+        document.turn = "O";
+        setMessage("It's " + document.turn + "'s turn!");
+    } else {
+        document.turn = "X";
+        setMessage("It's " + document.turn + "'s turn!");
+    }
+};
+
+checkWinner = (move) => {
+    let result = false;
+    if (checkRow(1, 2, 3, move) ||
+        checkRow(4, 5, 6, move) ||
+        checkRow(7, 8, 9, move) ||
+        checkRow(1, 4, 7, move) ||
+        checkRow(2, 5, 8, move) ||
+        checkRow(3, 6, 9, move) ||
+        checkRow(7, 8, 9, move) ||
+        checkRow(1, 5, 9, move) ||
+        checkRow(3, 5, 7, move)) {
         result = true;
-  }
-  return result;
-}
-function checkRow(sq1, sq2, sq3, move){
-  var result = false;
-  if(getBox(sq1)==move && getBox(sq2)==move && getBox(sq3)==move){
-      result = true;
-  }
-  return result;
-}
-function getBox(number){
-  return document.getElementById("s" + number).innerText;
-}
-function clearBox(number){
+    }
+    return result;
+};
+
+checkRow = (sq1, sq2, sq3, move) => {
+    let result = false;
+    if (getBox(sq1) === move && getBox(sq2) === move && getBox(sq3) === move) {
+        result = true;
+    }
+    return result;
+};
+
+getBox = (number) => {
+    return document.getElementById("s" + number).innerText;
+};
+
+clearBox = (number) => {
     document.getElementById("s" + number).innerText = "";
-}
+};
